764a9c7e25e106d37253d6cd8dbac2a973162eaa
Violet Stewart <vkazoo2@gmail.com>
2021-12-13 14:37:50 -0800

    fixed error message

--- src/App.js
+++ src/App.js
@@ -27,12 +27,13 @@ function App() {
 
     // 3. Set the resulting transformation as state using setFilms
     // 4. You'll know it works if the films show up on the page
-    return [];
     const resp = await fetch('https://the-one-api.dev/v2/movie/', {
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
       },
     });
+    const data = await resp.json();
+    console.log(data);
     return [resp];
   };
 
